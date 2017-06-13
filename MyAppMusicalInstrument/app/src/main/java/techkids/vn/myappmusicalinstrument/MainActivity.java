24242a225919a875d689f69ff25e622eb17d293f
MeoMunm <giahungtran1996@gmail.com>
2017-06-13 22:39:42 +0700

    UI for Drum Pad (clone)

---
+++ MyAppMusicalInstrument/app/src/main/java/techkids/vn/myappmusicalinstrument/MainActivity.java
@@ -0,0 +1,14 @@
+package techkids.vn.myappmusicalinstrument;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
